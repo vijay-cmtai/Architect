a3d5b14cbec7fb0c3661cfa984a4ba00ce095569
vijay-cmtai <vijay.maurya@completrix.com>
2025-12-06 19:42:33 +0530

    Update ContractorsSection.tsx

--- src/components/ContractorsSection.tsx
+++ src/components/ContractorsSection.tsx
@@ -255,7 +255,7 @@ const ContractorsSection: FC = () => {
 
   return (
     <>
-      <section className="py-20 bg-soft-teal">
+      <section className="py-15 bg-soft-teal">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center mb-12">
             <h2 className="text-4xl font-extrabold text-gray-800 tracking-tight">
