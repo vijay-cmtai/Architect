f95ef8f15560a8bec151c95dd9d42325334d0869
vijay-cmtai <vijay.maurya@completrix.com>
2025-12-07 10:47:21 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -43,7 +43,7 @@ const Index = () => {
       <CustomDesignSection />
       <FeaturedProducts />
       <HomeFloorPlans />
-      <FloorDElevations />
+      {/* <FloorDElevations /> */}
       {/* <CategoriesSection /> */}
       <ConstructionPartnersSection />
       <ContractorsSection />
